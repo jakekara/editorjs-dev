75295b08830f5fc1ffdc39a5eca0f4c623376d25
Jake Kara <jake@jakekara.com>
2020-09-17 09:52:11 -0400

    Added Babel

--- webpack.config.js
+++ webpack.config.js
@@ -1,10 +1,25 @@
 module.exports = {
-    entry: {
-      main: './src/index.js',
-    },
-    output: {
-      filename: '[name].js',
-      path: __dirname + '/dist'
-    }
-  };
-  
+  mode: "production",
+  entry: {
+    main: "./src/index.js",
+  },
+  output: {
+    filename: "[name].js",
+    path: __dirname + "/dist",
+  },
+  module: {
+    rules: [
+      {
+        test: /\.m?js$/,
+        exclude: /(node_modules|bower_components)/,
+        use: {
+          loader: "babel-loader",
+          options: {
+            presets: ["@babel/preset-env"],
+            plugins: ["@babel/plugin-proposal-object-rest-spread"],
+          },
+        },
+      },
+    ],
+  },
+};
