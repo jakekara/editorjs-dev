fbd861c65ac49bb22c4b15fe3ef84fe36020a0cf
Jake Kara <jake@jakekara.com>
2020-10-20 14:28:12 -0400

    Expose editor globally for debugging

--- src/index.ts
+++ src/index.ts
@@ -1,16 +1,15 @@
 import EditorJS from "@editorjs/editorjs";
 import Header from "@editorjs/header";
 
-// import { Footnote, FootnoteMaker } from '../editorjs-footnotes/src/index';
-// console.log("Fakely imported from package", FootnoteMaker, Footnote);
-
 import { Footnote, FootnoteMaker } from "editorjs-footnotes";
-console.log("Really imported from package", FootnoteMaker, Footnote);
+declare global {
+  interface Window { editor: EditorJS; }
+}
 
 const holderElement = document.createElement("div");
 holderElement.setAttribute("id", "editor-js-holder");
 document.body.appendChild(holderElement);
-new EditorJS({
+window.editor = new EditorJS({
   holder: "editor-js-holder",
   autofocus: true,
   tools: {
