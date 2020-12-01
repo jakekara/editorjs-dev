37d857a7dc2f807bfb85a11c636a13bb630b47d7
Jake Kara <jake@jakekara.com>
2020-12-01 11:43:00 -0500

    Super-charge MS Word Copy and paste

--- src/index.ts
+++ src/index.ts
@@ -1,12 +1,31 @@
 import EditorJS from '@editorjs/editorjs'
 import Header from '@editorjs/header'
+import sortBlocks from "editorjs-footnotes/src/blocksorter/sortBlocks"
 
-import { Footnote, FootnoteMaker } from 'editorjs-footnotes'
+import { Footnote, FootnoteMaker, TypedParagraph } from 'editorjs-footnotes'
 declare global {
   interface Window {
     editor: EditorJS
   }
 }
+
+// const wordPasteButton = document.createElement('button')
+// wordPasteButton.innerHTML = "Import"
+// wordPasteButton.onclick = () => {
+//   console.log("Click")
+//   navigator.clipboard.readText().then(content => {
+//     console.log("Clipboard content", content)
+//   })
+// }
+// document.body.appendChild(wordPasteButton)
+
+const sortButton = document.createElement('button')
+sortButton.innerHTML = "Sort"
+sortButton.onclick = () => {
+
+  sortBlocks(window.editor)
+}
+document.body.appendChild(sortButton)
 
 const holderElement = document.createElement('div')
 holderElement.setAttribute('id', 'editor-js-holder')
@@ -17,9 +36,14 @@ window.editor = new EditorJS({
   tools: {
     header: Header,
     FootnoteMaker,
+    paragraph: {
+      class: TypedParagraph,
+      inlineToolbar: true
+    },
     footnoteParagraph: {
       class: Footnote,
       inlineToolbar: ['link', 'bold', 'italic'],
     },
   },
 })
+
